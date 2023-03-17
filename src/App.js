c6047a5177329b4718c38febb9f54451fe7b58c7
Victor Gila Barreto <gilabarreto@gmail.com>
2023-03-17 10:49:15 -0600

    Fecth Open Weather data.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import './App.css';
 
 import axios from 'axios';
 
+import { useEffect } from 'react';
+
 function App() {
+
+  const openWeather = `https://api.openweathermap.org/data/2.5/weather?q=calgary&appid=${process.env.REACT_APP_WEATHER_KEY}&units=metric`;
+
+  const fetchData = () => {
+    axios.get(openWeather).then((res) => {
+      console.log(res.data);
+    })
+  }
+
+  useEffect(() => {
+    fetchData();
+  })
+
   return (
     <>
-    <h1>Hello world!</h1>
+      <h1>Hello world!</h1>
     </>
   );
 }
