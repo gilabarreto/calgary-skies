e9d5e3dbb0df6846e33715a30e49e5f0d9b80cd5
Victor Gila Barreto <gilabarreto@gmail.com>
2023-03-17 16:00:48 -0600

    Create formattedDate and formattedTime functions

--- src/App.js
+++ src/App.js
@@ -2,25 +2,47 @@ import './App.css';
 
 import axios from 'axios';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [weather, setWeather] = useState([]);
+
   const openWeather = `https://api.openweathermap.org/data/2.5/weather?q=calgary&appid=${process.env.REACT_APP_WEATHER_KEY}&units=metric`;
 
-  const fetchData = () => {
-    axios.get(openWeather).then((res) => {
-      console.log(res.data);
-    })
-  }
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(openWeather);
+      setWeather(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     fetchData();
-  })
+  }, [])
+
+  const formattedDate = () => {
+    const currentDate = new Date()
+    const dateOptions = { weekday: 'long', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    return currentDate.toLocaleDateString('en-US', dateOptions).replace(' PM', 'pm').replace('at','@');
+  }
+
+  const formattedTime = (time) => {
+    const currentTime = new Date(time * 1000);
+    const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true }
+    return currentTime.toLocaleTimeString('en-US', timeOptions).replace(' PM', 'pm');
+  }
 
   return (
     <>
-      <h1>Hello world!</h1>
+      <h1>#Calgar-Skies</h1>
+      <h1>{weather?.main?.temp}</h1>
+      <h2>{weather?.main?.temp_min} / {weather?.main?.temp_max} {`(feels like ${weather?.main?.feels_like})`}</h2>
+      <h2>{formattedDate()}</h2>
+      <h2>Sunrise: {formattedTime(weather?.sys?.sunrise)}</h2>
+      <h2>Sunset: {formattedTime(weather?.sys?.sunset)}</h2>
     </>
   );
 }
